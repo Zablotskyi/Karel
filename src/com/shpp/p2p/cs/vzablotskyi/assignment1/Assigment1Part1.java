79f3f86c032535d57802a9dccb646fd543b0e59e
Zablotskyi <zablotskyivv@gmail.com>
2023-11-27 22:35:36 +0200

    download all project and move robot. Create main method

---
+++ src/com/shpp/p2p/cs/vzablotskyi/assignment1/Assigment1Part1.java
@@ -0,0 +1,12 @@
+package com.shpp.p2p.cs.vzablotskyi.assignment1;
+
+import com.shpp.karel.KarelTheRobot;
+
+public class Assigment1Part1 extends KarelTheRobot {
+    @Override
+    public void run() throws Exception {
+        for (int i = 0; i < 5; i++) {
+            move();
+        }
+    }
+}
