1c7e86d6abf9db4b680ef12af17c1d266de87cf6
Zablotskyi <zablotskyivv@gmail.com>
2023-11-28 22:33:14 +0200

    downloaded the library Karel

--- src/com/shpp/p2p/cs/vzablotskyi/assignment1/Assigment1Part1.java
+++ src/com/shpp/p2p/cs/vzablotskyi/assignment1/Assigment1Part1.java
@@ -5,8 +5,16 @@ import com.shpp.karel.KarelTheRobot;
 public class Assigment1Part1 extends KarelTheRobot {
     @Override
     public void run() throws Exception {
-        for (int i = 0; i < 5; i++) {
-            move();
-        }
+        move();
+        pause();
+        turnLeft();
+        pickBeeper();
+        pickBeeper();
+        pickBeeper();
+        pickBeeper();
+        move();
+        move();
+        move();
+
     }
 }
