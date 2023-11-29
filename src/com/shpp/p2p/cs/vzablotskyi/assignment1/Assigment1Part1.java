6fa6d1c381e2cac84491ba359f248006b1e11e6e
Zablotskyi <zablotskyivv@gmail.com>
2023-11-29 20:34:55 +0200

    added methods pickTheNewspaper() and goHome()

--- src/com/shpp/p2p/cs/vzablotskyi/assignment1/Assigment1Part1.java
+++ src/com/shpp/p2p/cs/vzablotskyi/assignment1/Assigment1Part1.java
@@ -2,18 +2,10 @@ package com.shpp.p2p.cs.vzablotskyi.assignment1;
 
 import com.shpp.karel.KarelTheRobot;
 
-public class Assigment1Part1 extends KarelTheRobot {
+public class Assigment1Part1 extends HelperClass {
     @Override
     public void run() throws Exception {
-        move();
-        pause();
-        turnLeft();
-        pickBeeper();
-        pickBeeper();
-        pickBeeper();
-        pickBeeper();
-        move();
-        move();
-        move();
+        pickTheNewspaper();
+        goHome();
     }
 }
--- src/com/shpp/p2p/cs/vzablotskyi/assignment1/HelperClass.java
+++ src/com/shpp/p2p/cs/vzablotskyi/assignment1/HelperClass.java
@@ -1,4 +1,43 @@
 package com.shpp.p2p.cs.vzablotskyi.assignment1;
 
-public class HelperClass {
+import com.shpp.karel.KarelTheRobot;
+
+public class HelperClass extends KarelTheRobot {
+    // the method turns Karel 90 degrees clockwise
+    public void turnRight() throws Exception {
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
+
+    // the method turns Karel 180 degrees clockwise
+    public void turnAround() throws Exception {
+        turnLeft();
+        turnLeft();
+    }
+
+    // The method allows you to go and pick up a newspaper that is lying
+    // on the doorstep outside the door
+    public void pickTheNewspaper() throws Exception {
+        turnRight();
+        move();
+        turnLeft();
+        move();
+        move();
+        move();
+        move();
+        pickBeeper();
+    }
+
+    public void goHome() throws Exception {
+        turnAround();
+        move();
+        move();
+        move();
+        move();
+        turnRight();
+        move();
+        turnRight();
+        putBeeper();
+    }
 }
