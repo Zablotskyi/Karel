dc2b31bb3d53bd6fcbf0da86ef19f66e80b9bb62
Zablotskyi <zablotskyivv@gmail.com>
2023-12-05 18:00:26 +0200

    parent derectories have been changed

--- src/com/shpp/p2p/cs/vzablotskyi/assignment1/Assigment1Part1.java
+++ src/com/shpp/p2p/cs/vzablotskyi/assignment1/Assigment1Part1.java
@@ -1,7 +1,5 @@
 package com.shpp.p2p.cs.vzablotskyi.assignment1;
 
-import com.shpp.karel.KarelTheRobot;
-
 public class Assigment1Part1 extends HelperClass {
     @Override
     public void run() throws Exception {
